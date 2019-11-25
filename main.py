6a84902f54982a84654d0338dd418d6ba341d48a
Daniil <dan-trofimov@mail.ru>
2019-11-26 02:01:19 +0300

    add query vk api query

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('ok')
+import requests
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+token = getenv('VK_TOKEN')
+owner_id = getenv('GROUP_ID')
+
+url = f'https://api.vk.com/method/wall.get?access_token={token}&owner_id=-{owner_id}&v=5.103'
+
+res = requests.get(url)
+print(res.text)

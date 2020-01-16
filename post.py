db2795698044fd9a7958cfb7a73d392c2f39773a
Daniil <dan-trofimov@mail.ru>
2020-01-17 02:15:14 +0300

    Logging, new table field

--- post.py
+++ post.py
@@ -8,6 +8,7 @@ class Post(Model):
     id = IntegerField()
     date = DateTimeField()
     text = TextField()
+    posted = BooleanField()
     # I know, that sucks, but I will store poll as json
     poll = TextField()
 
@@ -19,21 +20,31 @@ class Post(Model):
     def update_posts(posts):
         for post in posts:
             if post['marked_as_ads']:
+                print('Post {} is ad, skipping'.format(post['id']))
                 continue
 
             if not len(post['attachments']):
+                print('Post {} has no attachments, skipping'.format(post['id']))
                 continue
 
             if post['attachments'][0]['type'] != 'poll':
+                print('Post {} contains no poll, skipping'.format(post['id']))
                 continue
 
-            ok = Post(
+            if Post.exists(post['id']):
+                print('Post {} already exists, skipping'.format(post['id']))
+                continue
+
+            Post.create(
                 id=post['id'],
                 date=post['date'],
                 text=post['text'],
-                poll=dumps(post['attachments'][0]['poll'])
+                poll=dumps(post['attachments'][0]['poll']),
+                posted=False
             )
-            print('Start saving')
-            ok.save()
 
-            exit(0)
+    @staticmethod
+    def exists(post_id):
+        posts = Post.select().where(Post.id == post_id)
+
+        return bool(posts)
--- vk.py
+++ vk.py
@@ -3,7 +3,7 @@ from json import loads
 
 
 def get_posts_from_vk(token, group_id):
-    url = f'https://api.vk.com/method/wall.get?access_token={token}&owner_id={group_id}&v=5.103&count=1'
+    url = f'https://api.vk.com/method/wall.get?access_token={token}&owner_id={group_id}&v=5.103&count=2'
     response = requests.get(url)
 
     data = loads(response.text)

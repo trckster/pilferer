2966bffd5b146fc94d82cb4dd6f798bc2eb34439
Daniil <dan-trofimov@mail.ru>
2020-01-17 04:49:55 +0300

    Logging time

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from tg import publish_post_to_channel_if_needed
 from post import Post
 from os import path
 from post import db
+from datetime import datetime
 
 
 def main():
+    print()
+    print('Starting process at {}'.format(datetime.now()))
     env = dotenv_values()
 
     posts = get_posts_from_vk(env['VK_TOKEN'], env['VK_GROUP_ID'], env['VK_POSTS_COUNT_LOAD'])

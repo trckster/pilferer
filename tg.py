7b4737a7b22e1cb6fca72813cb1e91afb857ff7c
Daniil <dan-trofimov@mail.ru>
2020-01-17 04:04:52 +0300

    It is getting hot! Add polls posting

--- tg.py
+++ tg.py
@@ -1,7 +1,7 @@
 from requests import get
 from post import Post
 from peewee import *
-from json import loads
+from json import loads, dumps
 
 # Thx hidemy.name for free proxy. Let it be hardcoded for the time
 proxy_ip = '51.158.111.242:8811'
@@ -21,10 +21,11 @@ def publish_post_to_channel_if_needed(token, telegram_channel_id):
 
     print('Posting {}'.format(post.id))
 
-    response = get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text=testpost{}'.format(
-        token,
-        telegram_channel_id,
-        post.id), proxies=proxies)
+    make_pre_post_with_text_if_needed(post, token, telegram_channel_id)
+
+    query = construct_query_2(post, token, telegram_channel_id)
+
+    response = get(query, proxies=proxies)
 
     answer = loads(response.text)
 
@@ -33,6 +34,39 @@ def publish_post_to_channel_if_needed(token, telegram_channel_id):
         return
 
     Post.update(posted=True).where(Post.id == post.id).execute()
+
+
+def make_pre_post_with_text_if_needed(post, token, telegram_channel_id):
+    if not len(post.text):
+        return
+
+    query = construct_query_1(post, token, telegram_channel_id)
+
+    return get(query, proxies=proxies)
+
+
+def construct_query_1(post, token, telegram_channel_id):
+    domain = 'https://api.telegram.org/'
+    bot_secret = 'bot{}'.format(token)
+
+    message = post.text
+
+    query = f'{domain}{bot_secret}/sendMessage?chat_id={telegram_channel_id}&text={message}'
+
+    return query
+
+
+def construct_query_2(post, token, telegram_channel_id):
+    domain = 'https://api.telegram.org/'
+    bot_secret = 'bot{}'.format(token)
+
+    question = 'What?'
+
+    options = dumps(['First one', 'Second one'])
+
+    query = f'{domain}{bot_secret}/sendPoll?chat_id={telegram_channel_id}&question={question}&options={options}'
+
+    return query
 
 
 def get_last_unpublished_post():
--- vk.py
+++ vk.py
@@ -3,7 +3,7 @@ from json import loads
 
 
 def get_posts_from_vk(token, group_id):
-    url = f'https://api.vk.com/method/wall.get?access_token={token}&owner_id={group_id}&v=5.103&count=2'
+    url = f'https://api.vk.com/method/wall.get?access_token={token}&owner_id={group_id}&v=5.103&count=10'
     response = requests.get(url)
 
     data = loads(response.text)
